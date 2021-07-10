65c3861f6951653fd67ddce604473eb161462238
Irie Aoi <eai@mizle.net>
2021-07-10 10:40:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pulsoid Client
+
+Custom client for [Pulsoid](https://pulsoid.net/).
+
+## Usage
+
+### Get ws URL
+
+1. Launch the default widget.
+  At first it is recommended to start Demo to make sure it works.
+2. Open DevTools.
+3. Open Network tab.
+4. Enable WS on the filter.
+5. Refresh the page to see one URL.
+  ![](https://i.imgur.com/zl0pNxc.png)
+6. Right click and select `Copy` -> `Copy link address`.
+
+### Open in Client
+
+1. Open `https://pulsoid-client.vercel.app/?ws=` + `YOUR WS URL HERE` in browser.
+2. If there is no problem, the heart rate will be displayed.
+3. The same URL can be displayed in the browser source of the broadcast software.
---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+import React from "react";
+import useWebSocket from "react-use-websocket";
+
+export const App = () => {
+    const socketUrl = new URL(document.URL).searchParams.get("ws");
+
+    if (socketUrl) {
+        return <Card socketUrl={socketUrl} />;
+    }
+
+    return <div>/?ws=<code>websocket url here</code></div>;
+};
+
+export default App;
+
+const Card: React.VFC<{ socketUrl: string }> = ({ socketUrl }) => {
+    const { lastMessage } = useWebSocket(socketUrl);
+
+    if (lastMessage === null) return <div>no message</div>;
+
+    const parsed = JSON.parse(lastMessage.data);
+    const heartRate = parsed.data.heartRate as number;
+
+    const green = 0 <= heartRate && heartRate < 100;
+    const yellow = 100 <= heartRate && heartRate < 140;
+    const red = 140 <= heartRate;
+
+    return (
+        <div className="flex p-4 pr-3">
+            <div
+                className={clsx(
+                    "relative flex rounded-lg shadow-lg ml-auto",
+                    "bg-white text-gray-800 text-xl font-bold uppercase leading-none"
+                )}
+            >
+                <div className="relative flex flex-col p-4 space-y-1 overflow-hidden">
+                    <div
+                        className={clsx(
+                            "absolute -bottom-4 w-2/6 opacity-0 transition-opacity duration-500",
+                            red && "opacity-60"
+                        )}
+                    >
+                        <img src="https://media.stellaria.network/custom_emojis/images/000/013/273/original/4aa472b7fe843dd6.png" />
+                    </div>
+
+                    <span className={"text-left"}>heart rate</span>
+                    <span className={"text-right text-6xl"}>{heartRate}</span>
+                    <span className={"text-right text-gray-500"}>bpm</span>
+                </div>
+            </div>
+        </div>
+    );
+};
